c0a8573b225ea3d564ed58e761eae85d7ff6fee3
liviubuciu2002 <liviubuciu2002@gmail.com>
2025-08-27 14:36:42 +0300

    load balancer aws

--- service1/src/main/java/com/liviu/Service1Application.java
+++ service1/src/main/java/com/liviu/Service1Application.java
@@ -1,9 +1,8 @@
-package com;
+package com.liviu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.reactive.function.client.WebClient;
 
@@ -14,9 +13,4 @@ public class Service1Application {
         SpringApplication.run(Service1Application.class, args);
     }
 
-    @Bean
-    @LoadBalanced
-    public WebClient.Builder loadBalancedWebClient() {
-        return WebClient.builder();
-    }
 }
